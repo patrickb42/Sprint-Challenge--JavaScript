cb1b5f662eab909b05b017d3e8debe3427add7d1
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-06-14 12:46:07 -0400

    everything plus stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,10 +1,58 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+/* ===== Prototype Practice ===== */
+
+// Task: You are to build a cuboid maker that can return values for
+//   a cuboid's volume or surface area.
+// Cuboids are similar to cubes but do not have even sides.
+// Follow the steps in order to accomplish this challenge.
+
+/* == Step 1: Base Constructor ==
+  Create a constructor function named CuboidMaker that accept
+  properties for length, width, and height
+*/
+class Cuboid {
+  constructor(spec) {
+    this.length = spec.length;
+    this.width = spec.width;
+    this.height = spec.height;
+  }
+
+  volume() {
+    return this.length * this.width * this.height;
+  }
+
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}
+
+const cuboid = new Cuboid({ length: 4, width: 5, height: 5 });
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.
 // Find out the formulas for volume and surface area for cubes and create those methods
 //    using the dimension properties from CuboidMaker.
+class CubeMaker extends Cuboid {
+  constructor(spec) {
+    super({ length: spec.size, width: spec.size, height: spec.size });
+    this.size = spec.size;
+  }
+
+  volume() {
+    return this.size ** 3;
+  }
+
+  surfaceArea() {
+    return 6 * (this.size ** 2);
+  }
+}
+
 // Test your work by logging out your volume and surface area.
+const cube = new CubeMaker({ size: 5 });
+
+console.log(cube.volume()); // 125
+console.log(cube.surfaceArea()); // 150
